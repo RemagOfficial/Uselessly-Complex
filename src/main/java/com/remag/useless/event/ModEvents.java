5498f0020e50374bb380fa3028be4ff547a4a1e7
RemagOfficial <52126530+RemagOfficial@users.noreply.github.com>
2022-08-17 23:43:24 +0100

    made damage from mining logs with a fist scale with difficulty

--- src/main/java/com/remag/useless/event/ModEvents.java
+++ src/main/java/com/remag/useless/event/ModEvents.java
@@ -14,8 +14,13 @@ public class ModEvents
         if(event.getState().getBlock().is(ModTags.Blocks.LOGS))
         {
             if(!event.getPlayer().getMainHandItem().getItem().is(ModTags.Items.AXES)) {
-                event.getPlayer().hurt(DamageSource.FLY_INTO_WALL, 1.0f);
-                event.setCanceled(true);
+                if (event.getWorld().getDifficulty().getId() == 0) {
+                    event.getPlayer().hurt(DamageSource.FLY_INTO_WALL, 2.0f);
+                    event.setCanceled(true);
+                } else {
+                    event.getPlayer().hurt(DamageSource.FLY_INTO_WALL, (2.0f * event.getWorld().getDifficulty().getId()));
+                    event.setCanceled(true);
+                }
             }
         }
     }
